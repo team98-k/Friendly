be00fe49281761e81dcee32c3b2dd4aea7f03f08
Kim <wow4090@gmail.com>
2022-11-02 11:39:52 +0900

    test1102

---
+++ lib/screen/home_screen.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/material.dart';
+
+import '../model/model_movie.dart';
+import '../widget/box_slider.dart';
+import '../widget/carousel_slider.dart';
+import '../widget/circle_slider.dart';
+
+class HomeScreen extends StatefulWidget {
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  List<Movie> movies = [
+    Movie.fromMap({
+      'title': '사랑의 불시착',
+      'keyword': '사랑/로맨스/판타지',
+      'poster': 'test_movie_1.png',
+      'like': false
+    }),
+    Movie.fromMap({
+      'title': '사랑의 불시착',
+      'keyword': '사랑/로맨스/판타지',
+      'poster': 'test_movie_1.png',
+      'like': false
+    }),
+    Movie.fromMap({
+      'title': '사랑의 불시착',
+      'keyword': '사랑/로맨스/판타지',
+      'poster': 'test_movie_1.png',
+      'like': false
+    }),
+    Movie.fromMap({
+      'title': '사랑의 불시착',
+      'keyword': '사랑/로맨스/판타지',
+      'poster': 'test_movie_1.png',
+      'like': false
+    }),
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: <Widget>[
+        Stack(
+          children: <Widget>[
+            CarouselImage(movies: movies),
+            TopBar(),
+          ],
+        ),
+        CircleSlider(movies: movies),
+        BoxSlider(movies: movies),
+      ],
+    );
+  }
+}
+
+class TopBar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.fromLTRB(20, 7, 20, 7),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          Image.asset(
+            'images/kbo.png',
+            fit: BoxFit.contain,
+            height: 25,
+          ),
+          Container(
+            padding: const EdgeInsets.only(right: 1),
+            child: const Text(
+              '알림 설정',
+              style: TextStyle(fontSize: 14),
+            ),
+          ),
+          Container(
+            padding: const EdgeInsets.only(right: 1),
+            child: const Text(
+              '좋아요',
+              style: TextStyle(fontSize: 14),
+            ),
+          ),
+          Container(
+            padding: const EdgeInsets.only(right: 1),
+            child: const Text(
+              '구독',
+              style: TextStyle(fontSize: 14),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/widget/bottom_bar.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class Bottom extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.black,
+      child: Container(height: 50,
+        child: const TabBar(
+          labelColor: Colors.white,
+          unselectedLabelColor: Colors.white60,
+          indicatorColor: Colors.transparent,
+          tabs: <Widget>[
+            Tab(icon: Icon(
+              Icons.home,
+              size: 18,
+            ),
+              child: Text("home", style: TextStyle(fontSize: 9)),
+            ),
+            Tab(icon: Icon(
+              Icons.list,
+              size: 18,
+            ),
+              child: Text("list", style: TextStyle(fontSize: 9)),
+            ),
+            Tab(icon: Icon(
+              Icons.shopping_cart,
+              size: 18,
+            ),
+              child: Text("shop", style: TextStyle(fontSize: 9)),
+            ),
+            Tab(icon: Icon(
+              Icons.location_on,
+              size: 18,
+            ),
+              child: Text("map", style: TextStyle(fontSize: 9)),
+            ),
+            Tab(icon: Icon(
+              Icons.pets_rounded,
+              size: 18,
+            ),
+              child: Text("profile", style: TextStyle(fontSize: 9)),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
