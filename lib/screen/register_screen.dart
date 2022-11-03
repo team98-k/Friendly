153f75d03ec5875dbc7ef6cdeedfc9520be7f45a
Minjeong An <105402566+Annopqr@users.noreply.github.com>
2022-11-03 21:27:08 +0900

    Update register_screen.dart

--- lib/screen/register_screen.dart
+++ lib/screen/register_screen.dart
@@ -2,14 +2,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'firebase_options.dart';
 
-void main() async{
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  runApp(Register());
-}
-
 class Register extends StatelessWidget{
-  const Register({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context){
     return MaterialApp(
