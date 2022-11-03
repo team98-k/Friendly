f0044b50b406acc165af0ef69f55d3114e257d1e
Minjeong An <105402566+Annopqr@users.noreply.github.com>
2022-11-03 20:48:26 +0900

    again2

--- lib/screen/register_screen.dart
+++ lib/screen/register_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:firebase_options.dart';
 
 
+
 class RegisterScreen extends StatelessWidget{
   @override
   Widget build(BuildContext context){
